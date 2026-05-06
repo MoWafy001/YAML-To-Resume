063bab0827974ae31b69e820909c62032ed48ae2
MoWafy001 <wafy123445@gmail.com>
2026-05-06 12:45:19 +0300

    feat: refactor command handling and improve structure for CLI commands

--- src/main.py
+++ src/main.py
@@ -4,19 +4,51 @@ from utils.configuration import get_configuration, has_no_configurations, initia
 from utils.pdf import create_resume_or_resumes
 
 
+def handle_init_command(cli_arguments):
+    initialize_reinitialize_configurations(cli_arguments.y)
+
+
+def handle_config_command(_):
+    print("Configuration files are located at:", CONFIG_DIR)
+
+
+def handle_build_command(cli_arguments):
+    create_resume_or_resumes(
+        cli_arguments.input_path,
+        cli_arguments.template,
+        cli_arguments.output
+    )
+
+
+def handle_new_command(cli_arguments):
+    if cli_arguments.new_type == "yaml":
+        raise NotImplementedError("YAML resume creation is not implemented yet.")
+    elif cli_arguments.new_type == "template":
+        raise NotImplementedError("Template resume creation is not implemented yet.")
+    else:
+        raise ValueError(f"Unknown new type: {cli_arguments.new_type}")
+
+
+def handle_command(cli_arguments):
+    command = {
+        "init": handle_init_command,
+        "config": handle_config_command,
+        "build": handle_build_command,
+        "new": handle_new_command
+    }.get(cli_arguments.command)
+
+    if command:
+        command(cli_arguments)
+    else:
+        raise ValueError(f"Unknown command: {cli_arguments.command}")
+
+
 def run():
     cli_arguments = parse_cli_arguments()
 
     # Initialize configurations if needed
-    if cli_arguments.command == "init" or has_no_configurations():
-        initialize_reinitialize_configurations(
-            interactive=cli_arguments.y is False
-        )
-        return
-    
-    # config mode
-    if cli_arguments.command == "config":
-        print("Configuration files are located at:", CONFIG_DIR)
+    if has_no_configurations():
+        initialize_reinitialize_configurations()
         return
 
     # Load configurations
@@ -24,16 +56,7 @@ def run():
         configs_file_path=cli_arguments.configs if 'configs' in cli_arguments else None
     )
 
-    # build mode
-    if cli_arguments.command == "build":
-        template_to_use = cli_arguments.template if cli_arguments.template else get_configuration('default_template')
-
-        create_resume_or_resumes(
-            cli_arguments.input_path,
-            template_to_use,
-            cli_arguments.output # can be None
-        )
-        return
+    handle_command(cli_arguments)
 
 if __name__ == "__main__":
     run()
